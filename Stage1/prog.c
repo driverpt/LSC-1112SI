14f42302e67ed04b463baf7a4873eccefae8a4aa
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2011-11-26 11:40:43 +0000

    Working Version of the printf

--- Stage1/prog.c
+++ Stage1/prog.c
@@ -16,6 +16,8 @@ typedef struct {
 
 typedef CharCell PCScreen[25][80];
 
+static char tempString[200];
+
 PCScreen * const screen = ( PCScreen* ) 0xb8000;
 
 extern char vbe_modes[];
@@ -24,7 +26,7 @@ struct vesa_mode_info * const modes = ( struct vesa_mode_info * ) ( vbe_modes + 
 #define SCREEN ( *screen )
 
 extern unsigned char bootdrv;
-
+/*
 void clear_screen() {
     unsigned x, y, i;
     for (y = 0; y < 25; ++y) {
@@ -43,17 +45,51 @@ void clear_screen() {
         }
     }  
 }
+*/
+char* itoa( int val ){
+  static char buf[32];
+  int i = 30, j, base = 10;
+  for(; val != 0; --i, val /= base) {
+      buf[i] = "0123456789"[val % base];
+  }
+  for( j = 0; buf[++i] != 0; ++j ) {
+      tempString[j] = buf[i];
+  }
+  tempString[j] = 0;
+  return tempString;
+}
+
+
+int strlen( const char * characters ) {
+  int count = 0;
+  for( ; characters[count] != 0; ++count );
+  return count;
+}
+
+void printXY( unsigned y, unsigned x, const char * string ) {
+    int i = 0;
+    for ( i = 0; i < strlen( string ); ++i ) {
+        SCREEN[y][x].fgcolor   = LGRAY;
+        SCREEN[y][x].bgcolor   = BLACK;
+        SCREEN[y][x].blink     = 0;
+        SCREEN[y][x + i].value = string[i];
+    }
+}
 
 void printXYHexa( unsigned y, unsigned x, int number ) {
-    SCREEN[y][x].value = number;
+    int i;
+    char * numberString = itoa( number );
+    printXY( y, x, numberString );
 }
 
 void vesa_modes_main() {
     unsigned i;
-    clear_screen();
+    //clear_screen();
+    
     for( i = 0; i < MAX_MODES; ++i ) {
-       printXYHexa( 12, 36 + i, modes[i].bpp + '0' );
+       printXYHexa( 12 + i, 36, modes[i].bpp );
     }
+    printXY( 12 + i, 36, "Done" );
 }
 /*
 void lsc_main() {
